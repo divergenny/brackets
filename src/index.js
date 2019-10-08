6efcd15bced55b6d75878cb93c276b105a1be662
Divergenny <divergenny@gmail.com>
2019-10-08 20:38:16 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = [];
+	let cacheOfArray = str;
+	for(let subarray = 0; subarray < bracketsConfig.length; subarray++){
+		array[subarray] = bracketsConfig[subarray][0] + bracketsConfig[subarray][1];
+	}
+	for (;;) {
+		let compare = cacheOfArray;
+		for ( let i = 0; i < bracketsConfig.length; i++ ) {
+			for (;;) {
+				let search = cacheOfArray.indexOf(array[i]);
+				if ( search >= 0 ) {
+					cacheOfArray = cacheOfArray.replace( array[i], "" );
+				}
+				else if ( search < 0) {
+					break;
+				}
+			}
+		}
+		if ( compare === cacheOfArray){
+			if(cacheOfArray == ""){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
 }
